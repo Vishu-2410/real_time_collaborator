0bcf28d092a5eb24d241c55f89bed383843b33d3
Vishakha Mourya <vishakhamourya508@gmail.com>
2025-11-23 09:08:35 +0530

    some chage for url

--- client/src/config/env.js
+++ client/src/config/env.js
@@ -1,4 +1,8 @@
+// export const env = {
+//   apiBaseUrl: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+//   socketUrl: import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000'
+// };
 export const env = {
-  apiBaseUrl: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
-  socketUrl: import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000'
+  apiBaseUrl: import.meta.env.VITE_API_URL || 'https://realtimecollaboratorapp.vercel.app//api',
+  socketUrl: import.meta.env.VITE_SOCKET_URL || 'https://realtimecollaboratorapp.vercel.app/'
 };
